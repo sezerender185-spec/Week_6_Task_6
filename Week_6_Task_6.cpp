97137ca2a73885f269901e858b4b23328354448a
sezerender185-spec <sezerender185@gmail.com>
2025-11-06 23:19:18 +0300

    Add files via upload

---
+++ Week_6_Task_6.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+int sumArray(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+          sum +=arr[i];
+       
+    }
+    return sum;
+}
+
+int main()
+{
+    int arr[] = { 1,2,3,4,5 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    std::cout << "Sum of elements "<< sumArray(arr, size);
+
+}
